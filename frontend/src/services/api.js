b2d689a8b6289d56def89d12015c883702e811b5
swayammohanty45 <swayammohanty26@gmail.com>
2026-06-04 16:46:36 +0530

    fix api exports

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,1 +1,13 @@
-import axios from 'axios' 
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'https://swayam-backend-yh2p.onrender.com/api',
+  timeout: 8000,
+})
+
+export const fetchProfile  = () => api.get('/profile/').then(r => r.data)
+export const fetchProjects = () => api.get('/projects/').then(r => r.data)
+export const fetchSkills   = () => api.get('/skills/').then(r => r.data)
+export const sendContact   = (data) => api.post('/contact/', data).then(r => r.data)
+
+export default api
